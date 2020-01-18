e64e593cced159c0b87342ce2ebbd7c6d0a3b70a
Kaito32768 <suzukikaito14.ne.jp@gmail.com>
2020-01-18 16:24:26 +0900

    update docker command

--- run_docker.sh
+++ run_docker.sh
@@ -1,2 +1,2 @@
 xhost local:
-sudo docker run -it --gpus all -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix -v $(pwd):/workspace --shm-size=2g cross32768/myrl-toolbox
+sudo docker run -it --gpus all -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix -v $(pwd):/workspace --shm-size=8g cross32768/myrl-toolbox
